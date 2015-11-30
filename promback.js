191e937186482aec5d9bd2d5d134b6ec58953d61
David Banham <david@banham.id.au>
2015-12-01 09:21:17 +1100

    Repro

---
+++ promback.js
@@ -0,0 +1,18 @@
+const foo = (barf) => {
+  return new Promise((resolve, reject) => {
+    if (barf) return reject(new Error('This is an error in foo'));
+    return resolve();
+  });
+}
+
+const bar = (err) => {
+  console.log('bar has been called', err);
+  throw new Error('OH NO');
+}
+
+foo()
+.then(() => {
+  console.log('foo has resolved');
+  bar();
+})
+.catch(bar);
